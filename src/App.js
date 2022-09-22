a93ae4b754bea96de2549312c4612d3016789f43
serhiy hvala <serhiyhvaladew@gmail.com>
2022-09-22 08:48:51 +0300

    modal window

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import { useState } from 'react';
-import './index.scss';
+import React from 'react'
+import { useState } from 'react'
+import './index.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [open, setOpen] = useState(false)
   return (
     <div className="App">
-      <div>
-        <h2>Counter:</h2>
-        <h1>{count}</h1>
-        <button className="minus" onClick={() => setCount(count - 1)}>- Minus</button>
-        <button className="plus" onClick={() => setCount(count + 1)}>Plus +</button>
+      <button className="open-modal-btn" onClick={() => setOpen(true)}>✨ Open Window</button>
+      <div className={`overlay animated ${open ? 'show' : ''}`}>
+        <div className="modal">
+          <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
+            <title />
+            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+          </svg>
+          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+        </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
